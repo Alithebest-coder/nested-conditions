f5aac0a8cc3e990bccfcc01b019ba9eb98b047ce
Alithebest-coder <aurumprint@proton.me>
2024-11-21 17:36:31 +1000

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,17 @@
+medical=input ("medical Y_N")
+att=int (input("att%"))
+if medical=="Y":
+    print("allowed")
+    
+    
+
+else :
+    
+    if(att>=75):
+        print("allowed")
+        
+        
+    else:
+        print ("not allowed")
+        
+        
---
+++ nested.py
@@ -0,0 +1,14 @@
+num=int(input("enter your number "))
+
+if num>=0:
+    if num==0:
+        print("num is 0")
+        
+    else:
+        print ("positive")
+        
+        
+        
+else:
+    print("negative")
+        
